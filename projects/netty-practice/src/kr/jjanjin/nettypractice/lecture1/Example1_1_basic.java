6bbb3a9183828be59af0f3a677d9bf1dd65866ef
JJangJin <jjy5146@naver.com>
2017-01-17 22:34:30 +0900

    책 기반 기존 소스 추가

---
+++ projects/netty-practice/src/kr/jjanjin/nettypractice/lecture1/Example1_1_basic.java
@@ -0,0 +1,35 @@
+package kr.jjanjin.nettypractice.lecture1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Example1_1_basic {
+
+	public static void main(String[] args) throws IOException {
+		ServerSocket serverSocket = new ServerSocket(3123);
+		
+		Socket clientSocket = serverSocket.accept();
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		
+		PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
+		
+		String request, response;
+		
+		while((request = in.readLine()) != null){
+			if("Done".equals(request)){
+				break;
+			}
+			
+			//response;
+			System.out.println(request);
+			out.println("out = " + request);
+		}
+
+	}
+
+}
